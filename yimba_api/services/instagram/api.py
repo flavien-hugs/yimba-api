f56e2b2c2c5ec682af36bc08b5e9e2e404b96eb7
haf <haf@incubtek.com>
2024-01-09 14:38:55 +0000

    remove slugify keyword to scrap data

--- yimba_api/services/instagram/api.py
+++ yimba_api/services/instagram/api.py
@@ -4,10 +4,9 @@ from typing import Optional
 from fastapi import HTTPException, Query, Security, status
 from fastapi_pagination import paginate
 
-from slugify import slugify
 from yimba_api.services.instagram import model
 from yimba_api.services import router_factory
-from yimba_api.shared import scrapper, crud, service
+from yimba_api.shared import scrapper, crud
 from yimba_api.shared.authentication import AuthTokenBearer
 
 logger = logging.getLogger(__name__)
@@ -65,19 +64,16 @@ async def search(
 
 @router.get(
     "/{keyword}",
+    dependencies=[Security(AuthTokenBearer(allowed_role=["admin", "client"]))],
     summary="Get Instagram hashtag",
 )
-async def get_instagram_hashtag(
-    keyword: str,
-    current_user: str = Security(AuthTokenBearer(allowed_role=["admin", "client"])),
-):
+async def get_instagram_hashtag(keyword: str):
     """
     Grattez et téléchargez des posts, profils, lieux, hashtags, photos et commentaires
     Instagram. Obtenez des données d'Instagram à l'aide d'un hashtag Instagram
     ou de requêtes de recherche.
     """
     try:
-        # await service.validate_project_exist(slugify(keyword), current_user)
         scraping = await scrapper.scrapping_instagram_data(keyword)
     except Exception as err:
         logger.error(f"An error occured: {err}")
