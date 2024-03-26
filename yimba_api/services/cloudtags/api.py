c173672475ddd53414480bc73111b5ccb6bdb33d
Flavien HUGS <flavienhugs@pm.me>
2024-03-26 08:35:20 +0000

    update cloud tags endpoint

--- yimba_api/services/cloudtags/api.py
+++ yimba_api/services/cloudtags/api.py
@@ -2,6 +2,7 @@ import logging
 import base64
 from io import BytesIO
 from fastapi import Security, HTTPException, status
+from fastapi.responses import JSONResponse
 
 from wordcloud import WordCloud
 from yimba_api.services import router_factory
@@ -24,22 +25,21 @@ def ping():
 
 @router.get(
     "/{keyword}",
-    dependencies=[Security(AuthTokenBearer(allowed_role=["admin", "client"]))],
+    # dependencies=[Security(AuthTokenBearer(allowed_role=["admin", "client"]))],
     status_code=status.HTTP_200_OK,
     summary="Generate cloud tags",
 )
 async def generate_cloudtags(keyword: str):
     try:
         text = await collect_keyword(keyword)
-        word_cloud = WordCloud(
+        wordcloud = WordCloud(
             collocations=False, background_color="white"
         ).generate_from_text(text)
-        image_bytes = BytesIO()
-        word_cloud.to_image().save(image_bytes, format="PNG")
-        base64_image = base64.b64encode(image_bytes.getvalue()).decode("utf-8")
 
-        result = "data:image/png;base64,{0}".format(base64_image)
-
+        result = JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"wordcloud": wordcloud}
+        )
     except Exception as exc:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exc)
